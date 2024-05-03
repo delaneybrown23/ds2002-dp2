a3c81022960e3d3140c2d16c54b2325250b58951
Delaney Brown <vvz2bg@virginia.edu>
2024-05-03 17:19:22 +0000

    Committing python script and count file to fork

---
+++ pyscriptdp2.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient, errors
+from bson.json_util import dumps
+import os
+import json
+
+MONGOPASS = os.getenv('MONGOPASS')
+uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
+client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
+# specify a database
+db = client.vvz2bg
+# specify a collection
+collection = db.dataproject2
+
+directory = "data"
+
+for filename in os.listdir(directory):
+	with open(os.path.join(directory, filename)) as f:
+		print(f)
+		try:
+			file_data = json.load(f)
+		except Exception as e:
+			print(e, "error when loading", f)
+		if isinstance(file_data, list):
+			try:
+				collection.insert_many(file_data)
+			except Exception as e:
+				print(e, "when importing into Mongo")
+		else:
+			try:
+				collection.insert_one(file_data)
+			except Exception as e:
+				print(e)
+				continue
